432228f6ea4505c81db8ce5166d0308e94100563
Princes Gail Martinez <princesgailm@gmail.com>
2026-08-13 14:07:02 +0800

    Changed landing page title to Welcome to Princes LavaLust

--- LavaLust/app/controllers/Welcome.php
+++ LavaLust/app/controllers/Welcome.php
@@ -3,7 +3,8 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class Welcome extends Controller {
 	public function index() {
-		$this->call->view('welcome_page');
-	}
+    $data['page_title'] = "Welcome to Princes LavaLust";
+    $this->call->view('welcome_page', $data);
+}
 }
 ?>
