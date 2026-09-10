5a91f596a216b998f373afff7cead3f028efd0e3
Princes Gail Martinez <princesgailm@gmail.com>
2026-09-10 15:00:41 +0800

    fix routes parameter mapping and enforce exact file casing

--- LavaLust/app/config/routes.php
+++ LavaLust/app/config/routes.php
@@ -1,46 +1,20 @@
 <?php
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-/*
-|--------------------------------------------------------------------------
-| HOME
-|--------------------------------------------------------------------------
-*/
-
+/* Home */
 $router->get('/', 'Welcome::index');
 
-
-/*
-|--------------------------------------------------------------------------
-| AUTHENTICATION
-|--------------------------------------------------------------------------
-*/
-
+/* Auth */
 $router->get('/login', 'AuthController::login');
 $router->post('/login', 'AuthController::login');
 $router->get('/logout', 'AuthController::logout');
 
-
-/*
-|--------------------------------------------------------------------------
-| PRODUCT CRUD
-|--------------------------------------------------------------------------
-*/
-
-/* Product List */
+/* Product CRUD */
 $router->get('/products', 'ProductController::index');
-
-/* Add Product Page */
 $router->get('/products/create', 'ProductController::create');
-
-/* Save Product */
 $router->post('/products/store', 'ProductController::store');
 
-/* Edit Product */
-$router->get('/products/edit/(:any)', 'ProductController::edit/$1');
-
-/* Update Product */
-$router->post('/products/update/(:any)', 'ProductController::update/$1');
-
-/* Delete Product */
-$router->get('/products/delete/(:any)', 'ProductController::delete/$1');
+/* Edit, Update, Delete Routes */
+$router->get('/products/edit/(:num)', 'ProductController::edit');
+$router->post('/products/update/(:num)', 'ProductController::update');
+$router->get('/products/delete/(:num)', 'ProductController::delete');
